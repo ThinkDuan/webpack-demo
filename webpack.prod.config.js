6841a55bacf2530f4ebc728d92b690a319bb299d
duanxinxin <daunxinxins@163.com>
2018-11-30 10:25:19 +0800

    change:删除不需要追踪的文件

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,8 +1,15 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const base = require('./webpack.base.config');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 module.exports = merge(base,{
+  devtool: 'source-map',
   plugins: [
-    new UglifyJSPlugin()
+    new UglifyJSPlugin({
+      sourceMap: true
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
   ]
 });
